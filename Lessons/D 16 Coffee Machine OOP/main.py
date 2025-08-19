1d36f800b95e891b36385ae44617b1ba6466de59
Alexander Masko <117868235+AlexOksam@users.noreply.github.com>
2025-08-19 21:33:32 +0200

    Create main.py

---
+++ Lessons/D 16 Coffee Machine OOP/main.py
@@ -0,0 +1,11 @@
+
+from menu import Menu
+from money_machine import MoneyMachine
+from coffee_maker import CoffeeMaker
+
+money_machine = CoffeeMaker()
+coffee_maker = MoneyMachine()
+
+
+money_machine.report()
+coffee_maker.report()
