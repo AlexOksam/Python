36958cd4ef4cd74dc9ab2037d4696f2e32c0e010
Alexander Masko <117868235+AlexOksam@users.noreply.github.com>
2025-08-24 23:11:12 +0200

    Create data.py

---
+++ Lessons/17. Quiz Project/data.py
@@ -0,0 +1,19 @@
+# https://opentdb.com/api.php?amount=10&difficulty=easy&type=boolean
+
+question_data  = [
+    {"type":"boolean",
+     "difficulty":"easy",
+     "category":"History",
+     "question":"The United States of America was the first country to launch a man into space.",
+     "correct_answer":"False",
+     "incorrect_answers":["True"]},
+     
+     {"type":"boolean","difficulty":"easy","category":"Entertainment: Video Games","question":"In &quot;Need for Speed: Porsche Unleashed&quot;, the player can only drive cars manufactured by Porsche.","correct_answer":"True","incorrect_answers":["False"]},
+     {"type":"boolean","difficulty":"easy","category":"History","question":"Adolf Hitler was a german soldier in World War I.","correct_answer":"True","incorrect_answers":["False"]},
+     {"type":"boolean","difficulty":"easy","category":"Science: Computers","question":"The Windows 7 operating system has six main editions.","correct_answer":"True","incorrect_answers":["False"]},
+     {"type":"boolean","difficulty":"easy","category":"Science: Computers","question":"The Python programming language gets its name from the British comedy group &quot;Monty Python.&quot;","correct_answer":"True","incorrect_answers":["False"]},
+     {"type":"boolean","difficulty":"easy","category":"Entertainment: Books","question":"Stephen Chbosky wrote the book &#039;The Perks of Being A Wallflower&#039;.","correct_answer":"True","incorrect_answers":["False"]},
+     {"type":"boolean","difficulty":"easy","category":"Science &amp; Nature","question":"Upwelling in the ocean provides colder nutrient rich water for plants and decomposers.","correct_answer":"True","incorrect_answers":["False"]},
+     {"type":"boolean","difficulty":"easy","category":"Entertainment: Video Games","question":"In Pok&eacute;mon Sun and Moon, a male Salandit can evolve to a Salazzle.","correct_answer":"False","incorrect_answers":["True"]},
+     {"type":"boolean","difficulty":"easy","category":"Entertainment: Board Games","question":"When setting up a chess board, the king is placed on a square of his own color.","correct_answer":"False","incorrect_answers":["True"]},
+     {"type":"boolean","difficulty":"easy","category":"Science &amp; Nature","question":"The Earth revolves around the Sun in one year.","correct_answer":"True","incorrect_answers":["False"]}]
