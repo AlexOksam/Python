b654115becd8c24691cedd12f52e0baa6ceb4889
Alexander Masko <117868235+AlexOksam@users.noreply.github.com>
2025-12-15 23:14:18 +0100

    Update 217_Contains_Duplicate.py

--- Leetcode/217_Contains_Duplicate.py
+++ Leetcode/217_Contains_Duplicate.py
@@ -8,6 +8,17 @@ class Solution:
                     return True
         return False
 
+# More faster
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        seen = set()
+        for num in nums:
+            if num in seen:
+                return True
+            seen.add(num)
+        return False
+
 
 """
 Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
