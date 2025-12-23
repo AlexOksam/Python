a14af7b391538d7b500f2c972e4b2dace410aa95
Alexander Masko <117868235+AlexOksam@users.noreply.github.com>
2025-12-23 10:14:15 +0100

    Update 1470_Shuffle_the_Array.py

--- Leetcode/1470_Shuffle_the_Array.py
+++ Leetcode/1470_Shuffle_the_Array.py
@@ -8,6 +8,17 @@ class Solution:
             answer.append(nums[i+n])
         return answer
 
+# SOLUTION with enumerate()
+
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        answer = []
+            for i, num in enumerate(nums):
+                if i < n:
+                    answer.append(nums[i])
+                    answer.append(nums[i+n])
+        return answer
+
 """
 Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
 Return the array in the form [x1,y1,x2,y2,...,xn,yn].
